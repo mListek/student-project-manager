1ee794f05f20a8486bb6c3b950d3cf2d29592e49
mListek <michal.listex@gmail.com>
2023-07-18 15:55:28 +0200

    Added frontend to cross origin policy

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/TaskRepository.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/TaskRepository.java
@@ -2,6 +2,8 @@ package com.listek.studentprojectmanager.dao;
 
 import com.listek.studentprojectmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
