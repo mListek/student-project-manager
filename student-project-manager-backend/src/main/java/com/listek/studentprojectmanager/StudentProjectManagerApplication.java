155294bf71b8383b40af4468916313e7d57db336
mListek <michal.listex@gmail.com>
2023-07-18 12:49:18 +0200

    Initialized Project

---
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/StudentProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.listek.studentprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProjectManagerApplication.class, args);
+	}
+
+}
---
+++ student-project-manager-backend/src/test/java/com/listek/studentprojectmanager/StudentProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listek.studentprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
