f27966fc7f4fdc62ff9965ddd6c9e776e57ba4b5
mListek <michal.listex@gmail.com>
2023-07-18 14:10:03 +0200

    Disabled PUT, POST, DELETE methods

---
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/config/DataRestConfig.java
@@ -0,0 +1,37 @@
+package com.listek.studentprojectmanager.config;
+
+import com.listek.studentprojectmanager.entity.Task;
+import com.listek.studentprojectmanager.entity.Team;
+import com.listek.studentprojectmanager.entity.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+    HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+    // disable HTTP methods for Team: PUT, POST, DELETE
+    config.getExposureConfiguration()
+        .forDomainType(Team.class)
+        .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    // disable HTTP methods for User: PUT, POST, DELETE
+    config.getExposureConfiguration()
+        .forDomainType(User.class)
+        .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    // disable HTTP methods for Task: PUT, POST, DELETE
+    config.getExposureConfiguration()
+        .forDomainType(Task.class)
+        .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+  }
+}
