2fd64f929213d5899a40a5df3d3a403b5493be64
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-18 21:33:04 +0200

    updated modules

--- student-project-manager-frontend/src/app/app.module.ts
+++ student-project-manager-frontend/src/app/app.module.ts
@@ -4,20 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TeamListComponent } from './components/team-list/team-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TeamService } from './services/team.service';
+import { FormsModule } from '@angular/forms';
+import { MessageListComponent } from './components/message-list/message-list.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamListComponent,
-    TaskListComponent
+    TaskListComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [TeamService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
