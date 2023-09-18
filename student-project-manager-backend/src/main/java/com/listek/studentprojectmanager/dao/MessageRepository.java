2cb155b06fa98ea2fb1f05d64416ac80cb06dbc7
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-18 13:05:47 +0200

    created repository

---
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.listek.studentprojectmanager.dao;
+
+import com.listek.studentprojectmanager.entity.Message;
+import com.listek.studentprojectmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
