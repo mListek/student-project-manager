87150a347204f21c454fde714a99366ab8ed1956
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-20 15:30:31 +0200

    creted component

---
+++ student-project-manager-frontend/src/app/components/group-details-page/group-details-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-details-page',
+  templateUrl: './group-details-page.component.html',
+  styleUrls: ['./group-details-page.component.css']
+})
+export class GroupDetailsPageComponent {
+
+}
