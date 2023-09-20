96f3bf59700612b2451e2737c1bd747cf6f4be1d
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-20 21:51:51 +0200

    added template form

--- student-project-manager-frontend/src/app/components/register-page/register-page.component.ts
+++ student-project-manager-frontend/src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  onRegister(form: NgForm) {
+    const value = form.value;
+    console.log(value);
+  }
 }
