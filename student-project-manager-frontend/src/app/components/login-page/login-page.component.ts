b1518f8edfbf19d372d42aa943abcd65647a19ac
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-20 21:52:15 +0200

    added template form

--- student-project-manager-frontend/src/app/components/login-page/login-page.component.ts
+++ student-project-manager-frontend/src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  onLogin(form: NgForm) {
+    const value = form.value;
+    console.log(value.email + ' - ' + value.password);
+    console.log(form);
+  }
 }
