f7d5b43e7a25fe3b13f2237d8cf350dbe0a8d321
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-22 11:36:19 +0200

    added new methods

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
@@ -4,6 +4,11 @@ import com.listek.studentprojectmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.Optional;
+
 @CrossOrigin("http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
