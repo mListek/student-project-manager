7263e2491b4494a6e284b749f7db5732f1335281
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-22 14:29:10 +0200

    removed unused method

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
@@ -4,11 +4,8 @@ import com.listek.studentprojectmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.Optional;
 
 @CrossOrigin("http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-
-    Boolean existsByEmail(String email);
+    User findByEmail(String email);
 }
