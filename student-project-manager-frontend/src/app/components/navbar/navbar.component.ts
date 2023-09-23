e027535295fc757c206b132e5e244f16f9314bcf
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-23 14:33:50 +0200

    added user subscription

--- student-project-manager-frontend/src/app/components/navbar/navbar.component.ts
+++ student-project-manager-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, take } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/model/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user!: User;
 
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit() {
+    this.authService.user.pipe(take(1)).subscribe(user => {
+      this.user = user;
+    });
+  }
 }
