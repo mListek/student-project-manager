4eab74b3090e89ee7fdbd3dd99a8f66f67e95159
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-26 13:20:27 +0200

    removed unused imports

--- student-project-manager-frontend/src/app/app.module.ts
+++ student-project-manager-frontend/src/app/app.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TeamListComponent } from './components/team-list/team-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MessageListComponent } from './components/message-list/message-list.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -22,9 +19,6 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    TeamListComponent,
-    TaskListComponent,
-    MessageListComponent,
     LoginPageComponent,
     RegisterPageComponent,
     NavbarComponent,
