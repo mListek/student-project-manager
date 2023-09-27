a478843a7017faa052e5d49298e7248f82cecfd9
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-27 11:45:09 +0200

    added members list fetching

--- student-project-manager-frontend/src/app/components/group-details-page/group-details-page.component.ts
+++ student-project-manager-frontend/src/app/components/group-details-page/group-details-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Team } from 'src/app/model/team.model';
 import { User } from 'src/app/model/user.model';
@@ -11,40 +11,42 @@ import { TeamService } from 'src/app/services/team.service';
   templateUrl: './group-details-page.component.html',
   styleUrls: ['./group-details-page.component.css']
 })
-export class GroupDetailsPageComponent implements OnInit {
-  user!: User;
+export class GroupDetailsPageComponent implements OnInit, OnDestroy {
+  user: User;
+  isTeacher = false;
   currentTeam: Team;
   teamMembers: User[];
 
   constructor(private authService: AuthService,
-              private route: ActivatedRoute,
               private teamService: TeamService) {}
   
   ngOnInit() {
-    // const teamId = +this.route.snapshot.paramMap.get('id');
-    // this.teamService.getMembersOfTeam(teamId).subscribe(res => {
-    //   this.teamMembers = res;
-    // },
-    // err => {
-    //   console.log('error');
-    //   console.log(err);
-    // });
-    // this.authService.user.subscribe(user => {
-    //   this.user = user;
-    //   if (user !== null) {
-    //     if (user.teams[0] !== undefined) {
-    //       this.currentTeam = user.teams[0];
-    //     }
-    //   }
-    // });
+    this.authService.user.subscribe(user => {
+      if (user !== null) {
+        this.user = user;
+        if (user.teams[0] !== undefined) {
+          this.currentTeam = user.teams[0];
+        }
+        if (user.role === 'teacher') {
+          this.isTeacher = true;
+        }
+      }
+    });
+    this.teamService.getMembersOfTeam(this.currentTeam.id).pipe(take(1)).subscribe(res => {
+      this.teamMembers = res;
+      console.log(this.teamMembers);
+    },
+    err => {
+      console.log(err);
+    });
   }
 
   onMemberDelete(userId: number) {
-    // this.teamService.deleteMember(userId, this.currentTeam.id).subscribe(res => {
-    //   console.log(res);
-    // },
-    // err => {
-    //   console.log(err);
-    // });
+    console.log('deleting user from group');
+    this.teamService.deleteMember(userId, this.currentTeam.id);
+  }
+
+  ngOnDestroy() {
+    this.isTeacher = false;
   }
 }
--- student-project-manager-frontend/src/app/services/team.service.ts
+++ student-project-manager-frontend/src/app/services/team.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,18 @@ export class TeamService {
   constructor(private http: HttpClient) { }
 
   getMembersOfTeam(id: number) {
-    // return this.http.get<User[]>(`${this.baseUrl}teams/${id}/users`);
+    return this.http.get<UsersResponse>(`${this.baseUrl}teams/${id}/users`).pipe(
+      map(res => res._embedded.users)
+    );
   }
 
   deleteMember(userId: number, teamId: number) {
-    // return this.http.delete(`${this.baseUrl}users/${userId}/teams/${teamId}`);
+    this.http.delete(`${this.baseUrl}users/${userId}/teams/${teamId}`);
+  }
+}
+
+interface UsersResponse {
+  _embedded: {
+    users: User[];
   }
 }
