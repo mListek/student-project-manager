d2e086932b59397f0f755b543b5d06964404e9f6
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-28 15:53:46 +0200

    added finding by id method

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/MessageRepository.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/MessageRepository.java
@@ -4,6 +4,9 @@ import com.listek.studentprojectmanager.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @CrossOrigin("http://localhost:4200")
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByTeamId(Long teamId);
 }
