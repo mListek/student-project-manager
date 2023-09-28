5fc07d802ec848994e2b709cfde225334e358250
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-28 19:38:19 +0200

    added task creation method

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/controller/TaskController.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/controller/TaskController.java
@@ -3,12 +3,16 @@ package com.listek.studentprojectmanager.controller;
 import com.listek.studentprojectmanager.dao.TaskRepository;
 import com.listek.studentprojectmanager.dao.TeamRepository;
 import com.listek.studentprojectmanager.dao.UserRepository;
-import com.listek.studentprojectmanager.dto.TaskRequest;
+import com.listek.studentprojectmanager.dto.TaskDto;
+import com.listek.studentprojectmanager.entity.Message;
 import com.listek.studentprojectmanager.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -24,15 +28,23 @@ public class TaskController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/teams/{teamId}/tasks")
-    public ResponseEntity<Task> createTask(@PathVariable(value = "teamId") long teamId,
-                           @RequestBody TaskRequest taskRequest) {
-        Task task = new Task();
-        task.setDescription(taskRequest.getDescription());
-        task.setStatus(taskRequest.getStatus());
-        task.setTeam(teamRepository.findById(teamId));
+    @GetMapping("/teams/{teamId}/tasks")
+    public ResponseEntity<List<Task>> getTasksByTeamId(@PathVariable(value = "teamId")Long teamId) {
+        if (!teamRepository.existsById(teamId)) {
+            throw new ResourceNotFoundException(("Not found Team with id = " + teamId));
+        }
 
-        task.addUser(userRepository.findById(taskRequest.getUserId()));
+        List<Task> tasks = taskRepository.findByTeamId(teamId);
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> createTask(@RequestBody TaskDto taskDto) {
+        Task task = new Task();
+        task.setDescription(taskDto.getDescription());
+        task.setTeam(teamRepository.findById(taskDto.getTeamId()));
+
+        task.addUser(userRepository.findById(taskDto.getUserId()));
 
         return new ResponseEntity<>(taskRepository.save(task), HttpStatus.OK);
     }
