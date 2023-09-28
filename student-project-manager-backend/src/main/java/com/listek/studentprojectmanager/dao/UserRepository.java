5164e032a4eeacb95bbddf223c517468e20ff821
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-28 10:41:27 +0200

    added findbyid method

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dao/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    User findById(long userId);
 }
