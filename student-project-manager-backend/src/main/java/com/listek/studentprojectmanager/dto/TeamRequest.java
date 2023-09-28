2a7b75dd69275d6101af9ed529efd525082b4391
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-28 14:18:04 +0200

    created dao

---
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dto/TeamRequest.java
@@ -0,0 +1,27 @@
+package com.listek.studentprojectmanager.dto;
+
+public class TeamRequest {
+    private String name;
+    private int userId;
+
+    public TeamRequest(String name, int userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
