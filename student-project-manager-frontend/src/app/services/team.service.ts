21a650f869f2b2cc72a4fb9bde6547088b0e621e
michal.listex@gmail.com <michal.listex@gmail.com>
2023-09-28 14:20:53 +0200

    implemented team getting methods

--- student-project-manager-frontend/src/app/services/team.service.ts
+++ student-project-manager-frontend/src/app/services/team.service.ts
@@ -1,18 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../model/user.model';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+import { Team } from '../model/team.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
+  currentTeam = new BehaviorSubject<Team>(null);
   private baseUrl = 'http://localhost:8080/api/';
 
   constructor(private http: HttpClient) { }
 
-  getMembersOfTeam(id: number) {
-    return this.http.get<UsersResponse>(`${this.baseUrl}teams/${id}/users`).pipe(
+  setCurrentTeam(team: Team) {
+    this.currentTeam.next(team);
+  }
+
+  createTeam(teamName: string, userId: number) {
+    console.log('create team teamservice');
+    return this.http.post(
+      `${this.baseUrl}teams`,
+      {
+        name: teamName,
+        userId: userId
+      }
+    )
+  }
+
+  getTeamsOfUser(userId: number) {
+    return this.http.get<TeamsResponse>(
+      `${this.baseUrl}users/${userId}/teams`).pipe(
+        map(res => res._embedded.teams)
+    );
+  }
+
+  getMembersOfTeam(teamId: number) {
+    return this.http.get<UsersResponse>(`${this.baseUrl}teams/${teamId}/users`).pipe(
       map(res => res._embedded.users)
     );
   }
@@ -25,5 +49,11 @@ export class TeamService {
 interface UsersResponse {
   _embedded: {
     users: User[];
+  }
+}
+
+interface TeamsResponse {
+  _embedded: {
+    teams: Team[];
   }
 }
