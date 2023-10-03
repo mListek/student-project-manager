f07106782ac12ec0804f2921ab340c9f74854980
michal.listex@gmail.com <michal.listex@gmail.com>
2023-10-03 12:09:01 +0200

    added id to the dao

--- student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dto/ResponseFile.java
+++ student-project-manager-backend/src/main/java/com/listek/studentprojectmanager/dto/ResponseFile.java
@@ -1,16 +1,27 @@
 package com.listek.studentprojectmanager.dto;
 
 public class ResponseFile {
+
+    private String id;
     private String name;
     private String url;
     private String type;
     private long size;
 
-    public ResponseFile(String name, String url, String type, long size) {
+    public ResponseFile(String id, String name, String url, String type, long size) {
+        this.id = id;
         this.name = name;
         this.url = url;
         this.type = type;
         this.size = size;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
